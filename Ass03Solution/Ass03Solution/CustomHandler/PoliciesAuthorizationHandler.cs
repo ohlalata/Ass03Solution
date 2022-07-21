b9711ffc5692d796d13670bfabb554cc13b40add
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-07-22 02:30:40 +0700

    PoliciesAuthorizationHandler

--- Ass03Solution/Ass03Solution/CustomHandler/PoliciesAuthorizationHandler.cs
+++ Ass03Solution/Ass03Solution/CustomHandler/PoliciesAuthorizationHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace Ass03Solution.CustomHandler
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Ass03Solution.CustomHandler
 {
-    public class PoliciesAuthorizationHandler
+    public class PoliciesAuthorizationHandler : AuthorizationHandler<CustomUserRequireClaim>
     {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, CustomUserRequireClaim requirement)
+        {
+            if (context.User == null || !context.User.Identity.IsAuthenticated)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            var claims = context.User.Claims;
+            var hasClaim = claims.Any(c => c.Type.Equals(requirement.ClaimType));
+            if (!hasClaim)
+            {
+                context.Fail();
+                return Task.CompletedTask;
+            }
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
     }
 }
