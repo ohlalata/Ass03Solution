f0aa718f6d92056902ef05ed01b3777bf6ab5bc0
HNGB <huynhngogiabao2211@gmail.com>
2022-07-22 02:28:37 +0700

    Add Authorization.. in CustomHandler

--- Ass03Solution/Ass03Solution/CustomHandler/AuthorizationPolicyBuilderExtension.cs
+++ Ass03Solution/Ass03Solution/CustomHandler/AuthorizationPolicyBuilderExtension.cs
@@ -1,6 +1,13 @@
-﻿namespace Ass03Solution.CustomHandler
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Ass03Solution.CustomHandler
 {
-    public class AuthorizationPolicyBuilderExtension
+    public static class AuthorizationPolicyBuilderExtension
     {
+        public static AuthorizationPolicyBuilder UserRequireCustomClaim(this AuthorizationPolicyBuilder builder, string claimType)
+        {
+            builder.AddRequirements(new CustomUserRequireClaim(claimType));
+            return builder;
+        }
     }
 }
