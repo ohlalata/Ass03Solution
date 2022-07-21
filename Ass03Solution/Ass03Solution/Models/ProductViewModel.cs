64c8eb6172d6eb2b5173f92835c9f98e76ef90b6
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-07-22 02:04:35 +0700

    Model product view

--- Ass03Solution/Ass03Solution/Models/ProductViewModel.cs
+++ Ass03Solution/Ass03Solution/Models/ProductViewModel.cs
@@ -1,6 +1,25 @@
-﻿namespace Ass03Solution.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ass03Solution.Models
 {
     public class ProductViewModel
     {
+        [Display(Name = "Product ID")]
+        public int ProductID { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Category")]
+        public string Category { get; set; }
+
+        [Display(Name = "Weight")]
+        public string Weight { get; set; }
+
+        [Display(Name = "Unit Price")]
+        public decimal UnitPrice { get; set; }
+
+        [Display(Name = "Units In Stock")]
+        public int UnitsInStock { get; set; }
     }
 }
