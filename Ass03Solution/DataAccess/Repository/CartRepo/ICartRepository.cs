989cc0500ab9ba0747918175430421d54ae6613d
HNGB <huynhngogiabao2211@gmail.com>
2022-07-22 01:04:01 +0700

    Add ICartRepository and CartRepository

--- Ass03Solution/DataAccess/Repository/CartRepo/ICartRepository.cs
+++ Ass03Solution/DataAccess/Repository/CartRepo/ICartRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Repository.CartRepo
 {
-    internal interface ICartRepository
+    public interface ICartRepository
     {
+        public Dictionary<int, CartProduct> GetCart();
+        public void AddToCart(int productId, int quantity, decimal price);
+        public void RemoveFromCart(int productId);
+        public void UpdateCart(int productId, int quantity, decimal price);
+        public void DeleteCart();
     }
 }
