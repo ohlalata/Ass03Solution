9444921e88f6c7fab199c44a8624ad20a4b1c2ea
ohlalata <nguyenbaotoan.8345@gmail.com>
2022-07-22 00:22:32 +0700

    order and orderdetail

--- Ass03Solution/BusinessObject/Order.cs
+++ Ass03Solution/BusinessObject/Order.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BusinessObject
 {
-    internal class Order
+    public class Order
     {
+        public Order()
+        {
+            OrderDetails = new HashSet<OrderDetail>();
+        }
+
+        public int OrderId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime? RequiredDate { get; set; }
+        public DateTime? ShippedDate { get; set; }
+        public decimal? Freight { get; set; }
+        public virtual Member Member { get; set; }
+        public virtual ICollection<OrderDetail> OrderDetails { get; set; }
     }
 }
