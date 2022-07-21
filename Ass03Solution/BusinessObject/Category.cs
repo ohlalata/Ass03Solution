884d7772de14c231c19ff6ee07de8a272d695a70
HuDery <duyhl0211@gmail.com>
2022-07-22 00:21:45 +0700

    Create ProductDetail.cs and Category.cs in BusinessObject

--- Ass03Solution/BusinessObject/Category.cs
+++ Ass03Solution/BusinessObject/Category.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace BusinessObject
 {
-    internal class Category
+    public partial class Category
     {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
